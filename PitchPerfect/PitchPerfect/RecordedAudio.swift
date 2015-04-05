6120544997a7dc4e475dd01e1cb619b52aaef445
pranayjj <Pranay.Jain@ge.com>
2015-04-04 20:52:59 -0500

    Committed by Udacity

---
+++ PitchPerfect/PitchPerfect/RecordedAudio.swift
@@ -0,0 +1,20 @@
+//
+//  RecordedAudio.swift
+//  PitchPerfect
+//
+//  Created by Jain, Pranay (GE Healthcare) on 4/4/15.
+//  Copyright (c) 2015 GE. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject{
+    var filePathUrl: NSURL!
+    var title: String!
+    
+    init(filePathUrl: NSURL, title: String) {
+        self.filePathUrl = filePathUrl
+        self.title = title
+    }
+}
+
